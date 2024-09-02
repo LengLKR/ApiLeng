6d3f4f54daa18cabb09669e584d83d092fca02ee
LengLKR <Lengkrupanit555@gmail.com>
2024-09-02 17:21:34 +0700

    ส่งข้อความแบบไม่มีคำหยาบได้แล้วมั้งเหลือเช็ค

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.get("/api/sendtoline", async (req, res) => {
 
 // ตั้งเวลาส่งข้อความทุกๆ 7 โมงเช้าเวลาไทย
 cron.schedule(
-  "0 7 * * *",
+  "21 17 * * *",
   async () => {
     try {
       const response = await axios.get("http://localhost:8888/api/sendtoline");
